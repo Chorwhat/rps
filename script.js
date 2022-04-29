8cf22fc708f9b6e6d3618fabbc1ad6d03164b932
Shing <chorwhat1@gmail.com>
2022-04-29 00:17:49 -0400

    add playRound function

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ function computerPlay(){
     }
     return computersChoice;
 }
+
+
+function playRound(playerSelection, computerSelection){
+    let playerChoice = playerSelection.toLowerCase();
+    
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
+        if(playerChoice === "rock"){
+            if(computerSelection === "Scissors"){
+                return "You Win! Rock beats Scissors!";
+            } else if (computerSelection === "Rock"){
+                return "Tie Game! You both chose Rock!";
+            } else {
+                return "You Lose! Rock loses to Paper!"
+            }
+        }
+        else if(playerChoice === "paper"){
+            if(computerSelection === "Rock"){
+                return "You Win! Paper beats Rock!";
+            } else if (computerSelection === "Paper"){
+                return "Tie Game! You both chose Paper!";
+            } else {
+                return "You Lose! Paper loses to Scissors!"
+            }
+        } else {
+            if(computerSelection === "Paper"){
+                return "You Win! Scissors beats Paper!";
+            } else if (computerSelection === "Scissors"){
+                return "Tie Game! You both chose Scissors!";
+            } else {
+                return "You Lose! Scissors loses to Rock!"
+            }
+        }
+    }else {
+        console.log("invalid player selection");
+    }
+}
