97e2a4aedf6cc0eb868fd22c62f60fc46427f131
Shing <chorwhat1@gmail.com>
2022-04-28 23:52:45 -0400

    Add computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-prompt("Whats crackin", "yo mommas ass");
+function computerPlay(){
+    let d3Roll = Math.floor(Math.random() * 3) + 1;
+    let computersChoice;
+
+    if(d3Roll === 1){
+        computersChoice = "Rock";
+    } else if(d3Roll === 2){
+        computersChoice = "Paper";
+    } else {
+        computersChoice = "Scissors";
+    }
+    return computersChoice;
+}
