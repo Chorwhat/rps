5dd794d547b01b85e64cdca43715d779e6c18185
Shing <chorwhat1@gmail.com>
2022-04-30 19:30:56 -0400

    change expected playerSelection and improve statCheck

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ let lose = "lose";
 let winCount = 0;
 let loseCount = 0;
 let tieCount = 0;
+let total = 0;
 let playerPaperCount = 0;
 let playerRockCount = 0;
 let playerScissorsCount = 0;
 let enemyPaperCount = 0;
 let enemyRockCount = 0;
 let enemyScissorsCount = 0;
+let victoriousRock = 0;
+let victoriousPaper = 0;
+let victoriousScissor = 0;
+let victoriousEnemyRock = 0;
+let victoriousEnemyPaper = 0;
+let victoriousEnemyScissor = 0;
+
 
 function setplayerSelection(){
     playerSelection = prompt();
@@ -24,13 +32,10 @@ function computerPlay(){
 
     if(d3Roll === 1){
         computersChoice = "Rock";
-        enemyRockCount++;
     } else if(d3Roll === 2){
         computersChoice = "Paper";
-        enemyPaperCount++;
     } else {
         computersChoice = "Scissors";
-        enemyScissorsCount++;
     }
     return computersChoice;
 }
@@ -40,41 +45,52 @@ function playRound(playerSelection, computerSelection){
     let playerChoice = playerSelection.toLowerCase();
     
     
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
-        if(playerChoice === "rock"){
+    if (playerChoice === "r" || playerChoice === "p" || playerChoice === "s"){
+        total++;
+        if(playerChoice === "r"){
             playerRockCount++;
             if(computerSelection === "Scissors"){
                 console.log( "You Win! Rock beats Scissors!");
                 winCount++;
+                enemyScissorsCount++;
+                victoriousRock++;
                 console.log("Wins: " + winCount);
                 return win;
             } else if (computerSelection === "Rock"){
                 console.log( "Tie Game! You both chose Rock!");
                 tieCount++;
+                enemyRockCount++;
                 console.log("Ties: " + tieCount);
                 return tie;
             } else {
                 console.log( "You Lose! Rock loses to Paper!");
                 loseCount++;
+                enemyPaperCount++;
+                victoriousEnemyPaper++;
                 console.log("Loses: " + loseCount);
                 return lose;
             }
         }
-        else if(playerChoice === "paper"){
+        else if(playerChoice === "p"){
             playerPaperCount++;
             if(computerSelection === "Rock"){
                 console.log( "You Win! Paper beats Rock!");
                 winCount++;
+                enemyRockCount++;
+                victoriousPaper++;
                 console.log("Wins: " + winCount);
                 return win;
             } else if (computerSelection === "Paper"){
                 console.log( "Tie Game! You both chose Paper!");
                 tieCount++;
+                enemyPaperCount++;
                 console.log("Ties: " + tieCount);
                 return tie;
             } else {
                 console.log( "You Lose! Paper loses to Scissors!");
                 loseCount++;
+                enemyScissorsCount++;
+                victoriousEnemyScissor++;
                 console.log("Loses: " + loseCount);
                 return lose;
             }
@@ -83,22 +99,27 @@ function playRound(playerSelection, computerSelection){
             if(computerSelection === "Paper"){
                 console.log( "You Win! Scissors beats Paper!");
                 winCount++;
+                enemyPaperCount++;
+                victoriousScissor++;
                 console.log("Wins: " + winCount);
                 return win;
             } else if (computerSelection === "Scissors"){
                 console.log( "Tie Game! You both chose Scissors!");
                 tieCount++;
+                enemyScissorsCount++;
                 console.log("Ties: " + tieCount);
                 return tie;
             } else {
                 console.log( "You Lose! Scissors loses to Rock!");
                 loseCount++;
+                enemyRockCount++;
+                victoriousEnemyRock++;
                 console.log("Loses: " + loseCount);
                 return lose;
             }
         }
     }else {
-        console.log("invalid player selection");
+        console.log("invalid player selection, Redo round: " + (loseCount + tieCount + winCount + 1));
     }
 }
 
@@ -109,25 +130,34 @@ function game(){
     
     while(!isOver){
     playRound(prompt(),computerPlay());
-    let total = winCount + loseCount + tieCount;
+    
     if(winCount == 5){
         console.log("oh shit you got me");
-        console.log("Total Rounds Played: " + total);
+        
+        statCheck();
         isOver = true;
     } else if (loseCount == 5){
         console.log("GOODBYE LOSER");
-        console.log("Total Rounds Played: " + total);
+        statCheck();
         isOver = true;
     }
     }
 }
 
 function statCheck(){
-    console.log("Rocks Thrown at the Enemy: " + playerRockCount);
-    console.log("Paper Flung at the Enemy: " + playerPaperCount);
-    console.log("Scissors Hurled at the Enemy: " + playerScissorsCount);
-    console.log("Rocks Thrown at the Player: " + enemyRockCount);
-    console.log("Paper Flung at the Player: " + enemyPaperCount);
-    console.log("Scissors Hurled at the Player: " + enemyScissorsCount);
+    console.log("");
+    console.log("------------- STAT CHECK -----------------");
+    console.log("Total Rounds Played: " + total);
+    console.log("Final Score: " + winCount +" : " + loseCount + " with " + tieCount + " ties");
+    console.log("------------- PLAYER SCORE ---------------");
+    console.log("");
+    console.log("Rocks Thrown at the Enemy: " + playerRockCount + " , " + victoriousRock + " of which were successful");
+    console.log("Paper Flung at the Enemy: " + playerPaperCount + " , " + victoriousPaper + " of which were successful");
+    console.log("Scissors Hurled at the Enemy: " + playerScissorsCount  + " , " + victoriousScissor + " of which were successful");
+    console.log("------------- ENEMY SCORE ----------------");
+    console.log("");
+    console.log("Rocks Thrown at the Player: " + enemyRockCount + " , " + victoriousEnemyRock + " of which were successful");
+    console.log("Paper Flung at the Player: " + enemyPaperCount + " , " + victoriousEnemyPaper + " of which were successful");
+    console.log("Scissors Hurled at the Player: " + enemyScissorsCount + " , " + victoriousEnemyScissor + " of which were successful");
 }
 
